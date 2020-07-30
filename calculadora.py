cd2bd2c3ff13c7db107f8935689333d4607f1fdd
igor dantas <68202697+igorxdantas@users.noreply.github.com>
2020-07-29 23:48:10 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,44 @@
+#n1 = int(input("qual o primeiro n1? "))
+#n2 = int (input("qual o segundo? "))
+#operacao = str(input("operacoes: +, -, /, *, ** "))
+
+#if operacao == "+":
+   # calculo = n1 + n2
+   # print ("o resultado é", calculo)
+#elif operacao == "-":
+   # calculo = n1 - n2
+  #  print ("o resultado é", calculo)
+#elif operacao == "/":
+   # calculo = n1 / n2
+   # print ("o resultado é", calculo)
+#elif operacao == "*":
+   # calculo = n1 * n2
+   # print ("o resultado é", calculo)
+
+#elif operacao == "**":
+   # calculo = n1 ** n2
+   # print ("o resultado é", calculo)
+    
+ #elif operacao == "r":
+ #   n2 = 0.5
+  #  calculo ** n1 ** n2
+  #  print ("raiz de é", r)   
+    
+num1 = int(input("qual o priemiro numero? "))
+num2 = int(input("qual o segundo numero?"))
+conta = str (input("qual será a operação?: +, -, /,* "))
+
+if conta== "+":
+    calculo = num1 + num2
+    print ("o resultado é ", calculo)
+elif conta== "-":
+    calculo = num1 - num2
+    print ("o resultado é ", calculo)
+elif conta== "/":
+    calculo = num1 / num2
+    print ("o resultado é ", calculo)
+elif conta=="*":
+    calculo = num1 * num2
+    print ("o resultado é ", calculo)
+
+        
---
+++ float.py
@@ -0,0 +1,13 @@
+
+#numerodepessoas = int(input("qual quant max de pessoas suportada?"))
+#pesopessoa = float(input("qual o peso de cada pessoa"))
+#pesomax = numerodepessoas*pesopessoa
+
+pessoa = float(input("qual a renda de cada pessoa"))
+
+
+
+print("obrigado")
+
+#print("o valor é", pesomax, "kg")
+
