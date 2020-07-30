0cb6a7789bf9bdc8544421dbe2df0baa6152e5dc
igor dantas <68202697+igorxdantas@users.noreply.github.com>
2020-07-29 23:48:45 -0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,30 @@
+#<18 menor de idade
+#maior de 18 ate 49 anos maior de idade
+#50 ano0s ouy mais idoso
+
+#idade = int(input("qual a sua idade? "))
+
+###
+#if idade == 18:
+#    print("usuario maior de idade")
+#elif idade >18:
+#    print("usarioo maior de 18+")
+#else:
+#    print("usuario menor de idade")
+    
+#if idade > 50:
+   # print("pessoa idosa")
+
+#elif idade > 18 and idade <= 49:
+   # print ("adulto")
+#else:
+    #print("usiario menor")
+
+
+renda = int(input("qual a sua renda? "))
+
+if renda < 1000:
+    print ("abixo do adequado")
+if renda >= 1001:
+    print("continue")
+    
--- int.py
+++ int.py
@@ -4,15 +4,19 @@ numero=5*2
 numero=5+2
 numero=int(input("insira o primeiro numero"))
 numero2=int(input("escreva o segundo numero"))
+n3=int(input("escreva o terceiro número"))
 #calculo=numero+numero2
 porcentagem=((numero+numero2)/100*50)
 
 
+
 print(numero)
 print(numero2)
+print(n3)
 #print(calculo)
 print("o resultado da subtração é", numero-numero2)
 print("o resultado da adição é", numero+numero2)
 print("o resultado da divição é", numero/numero2)
 print("o resultado da multiplicação é", numero*numero2)
 print("o resultado da porcentagem é", porcentagem)
+
